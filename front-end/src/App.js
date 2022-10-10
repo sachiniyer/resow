c34b18edc50ede9245fca75e8cf2b17bf0d3e8d5
Jacob Baum <jacoblbaum@gmail.com>
2022-10-10 13:28:08 -0400

    Add initial header and footer

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-body">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
