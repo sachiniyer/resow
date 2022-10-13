689f6ec9f872198c5d9082c220f43fcffc1f7380
tasnimahmed11 <ta1743@nyu.edu>
2022-10-12 20:57:03 -0400

    minor changes to UserProfile.js

--- front-end/src/pages/UserProfile.js
+++ front-end/src/pages/UserProfile.js
@@ -1,29 +1,8 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import SavedPost from "./SavedPost";
-import EditProfile from "./EditProfile";
-import PastUploads from "./PastUpload";
 
 function UserProfile() {
   return (
     <div className="UserProfile">
       <h1> User Profile Page </h1>
-
-
-      <Router>
-        <main className="Router">
-          <Routes>
-            {/* a route for the saved post page */}
-            <Route path="/SavedPost" element={<SavedPost />} />
-
-            {/* a route for the edit profile page */}
-            <Route path = "/EditProfile" element={<EditProfile/>} />
-
-            {/* a route for the messages page */}
-            <Route path = "/PastUploads" element={<PastUploads/>} />
-
-          </Routes>
-        </main>
-      </Router>
 
     </div>
   );
