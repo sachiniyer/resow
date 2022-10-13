931a52a5ce3a19918c7d95dfcddf7451116babe4
tasnimahmed11 <ta1743@nyu.edu>
2022-10-12 20:32:35 -0400

    Inserted the routing code to index.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,59 +1,12 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './App.css';
-
-
-import Homepage from './pages/Homepage';
-import UploadItem from './pages/UploadItem';
-import UserProfile from './pages/UserProfile';
-import Messages from './pages/Messages';
-
-
-import ItemList from './pages/ItemsList';
-import ItemDetails from './pages/ItemDetails';
-import PastUploads from './pages/PastUpload';
-import SavedPost from './pages/SavedPost';
-import ChatPage from './pages/ChatPage';  //must be fixed later
-
-
-
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <main className="App-main">
-          <Routes>
-            {/* a route for the home page */}
-            <Route path="/Homepage" element={<Homepage />} />
+      <header className="App-body">
+       
+      </header>
 
-            {/* a route for the upload item page */}
-            <Route path = "/UploadItem" element={<UploadItem/>} />
-
-            {/* a route for the user profile page */}
-            <Route path = "/UserProfile" element={<UserProfile/>} />
-
-            {/* a route for the messages page */}
-            <Route path = "/Messages" element={<Messages/>} />
-
-            {/* a route for the list page */}
-            <Route path = "/ItemsList" element={<ItemList/>} />
-
-            {/* a route for the item details page */}
-            <Route path = "/ItemsDetails" element={<ItemDetails/>} />
-
-            {/* a route for the past upload page */}
-            <Route path = "/PastUploads" element={<PastUploads/>} /> 
-
-            {/* a route for the saved post page */}
-            <Route path = "/SavedPost" element={<SavedPost/>} />
-
-            {/* a route for the chat inbox page */}
-            <Route path = "/User?ChatInbox" element={<ChatPage/>} />
-
-          </Routes>
-        </main>
-      </Router>
     </div>
   );
 }
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,6 +5,19 @@ import App from './App';
 import Header from './components/Header'
 import Footer from './components/Footer';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+import Homepage from './pages/Homepage';
+import UploadItem from './pages/UploadItem';
+import UserProfile from './pages/UserProfile';
+import Messages from './pages/Messages';
+
+
+import ItemList from './pages/ItemsList';
+import ItemDetails from './pages/ItemDetails';
+import PastUploads from './pages/PastUpload';
+import SavedPost from './pages/SavedPost';
+import ChatPage from './pages/ChatPage';  //must be fixed later
 
 
 
@@ -13,7 +26,25 @@ root.render(
   <React.StrictMode>
     <Header />
     <App />
-    <Footer />
+    <Router>
+      <main className="Router">
+        <Routes>
+          {/* a route for the home page */}
+          <Route path="/Homepage" element={<Homepage />} />
+
+          {/* a route for the upload item page */}
+          <Route path = "/UploadItem" element={<UploadItem/>} />
+
+          {/* a route for the user profile page */}
+          <Route path = "/UserProfile" element={<UserProfile/>} />
+
+          {/* a route for the messages page */}
+          <Route path = "/Messages" element={<Messages/>} />
+
+        </Routes>
+        </main>
+      </Router>
+      <Footer />
   </React.StrictMode>
 );
 
