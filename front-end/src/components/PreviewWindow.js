f0d054477d45c9fd7519525560a7d2efb24242f2
MinwuKim <minwu1221@gmail.com>
2022-10-13 17:18:30 -0400

    reduce window size

--- front-end/src/components/PreviewWindow.js
+++ front-end/src/components/PreviewWindow.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
@@ -10,35 +9,29 @@ import Avatar from '@mui/material/Avatar';
 
 export default function PreviewWindow(props) {
   return (
-    <Card sx={{ display: 'flex', width:0.9, maxWidth: 340, borderRadius: '5%' }}>
+    <Card sx={{ display: 'flex', width:0.9, maxWidth: 200, borderRadius: '5%' }}>
       <CardActionArea>
         <CardMedia 
           style={{ borderBottom: "solid"}}
           component="img"
-          height="345"
+          height="200"
           image="/resowLogo.png"
           alt="thumbnail"
         />
         <CardContent>
           <Box sx={{display: 'grid', gap: 0, gridTemplateColumns: 'repeat(2, 1fr)' }}>
-            <Box sx={{ width:103}}>
-                <Avatar sx={{ border: "solid", width: 100, height: 100}} alt="thumbnail" src={props.profileURL}/>
-                <Box sx={{ width:135, wordWrap: "break-word", marginTop: 0, marginLeft:-2}}>
-                    <Typography component="div" variant="body2" align="center">
+            <Box sx={{width:60}}>
+                <Avatar sx={{ border: "solid", width: 60, height: 60, marginLeft:-1, marginTop:-1}} alt="thumbnail" src={props.profileURL}/>
+                <Box sx={{width:80, wordWrap: "break-word", marginTop: 0, marginLeft:-2, fontSize: "0.1em"}}>
                      {props.sellerName}
-                    </Typography>
                 </Box>
             </Box>
-            <Box sx={{ width:190}}>
-                <Box sx={{width:180, wordWrap: "break-word", marginTop: 2, marginLeft:1}}>
-                    <Typography variant="body2" align="left" >
-                        {props.title}
-                    </Typography>
+            <Box sx={{width:120}}>
+                <Box sx={{width:100, wordWrap: "break-word", marginTop: 1, marginLeft:1, fontSize: 1, textAlign:"left"}}>
+                    {props.title}
                 </Box>
-                <Box sx={{  width:180, wordWrap: "break-word", marginTop: 2, marginLeft:1}}>
-                    <Typography variant="body2" color="text.secondary" align="left">
-                        {props.location}
-                    </Typography>
+                <Box sx={{ width:100, wordWrap: "break-word", marginTop: 1.5, marginLeft:1, fontSize: 1, textAlign:"left", color:"#2596be"}}>
+                    {props.location}
                 </Box>
             </Box>
           </Box>
