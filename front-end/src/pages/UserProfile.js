ea8324356d971aad7d0fffacae30867b19a1e0cf
tasnimahmed11 <ta1743@nyu.edu>
2022-10-16 21:04:42 -0400

    added user profile picture

--- front-end/src/pages/UserProfile.js
+++ front-end/src/pages/UserProfile.js
@@ -1,17 +1,37 @@
 import { Link } from "react-router-dom";
+import './UserProfile.css';
+import userimage from "./images/profilepic.jpeg"
+
+
+/*function UserImage(props){
+  return(
+    <div>
+      <img className = "UserImage" src="./images/profilepic.jpeg" alt={props.alt} height = "100px" width = "100px"/> 
+    </div>
+  )
+}*/
 
 
 function UserProfile() {
   return (
-    <div className="UserProfile">
-      <h1> User Profile Page </h1>
-      <ul>
-        <li> <Link to ="/PastUpload"> PastUpload </Link> </li>
-        <li> <Link to ="/UserProfile/SavedPost"> SavedPost </Link> </li>
-        <li> <Link to ="/UserProfile/EditProfile"> Edit Profile </Link> </li>
-      </ul>
-    </div>
-  );
+    <>
+      <div className="UserProfile">
+
+        <div className="Image">
+          <img className="ProfilePicture" src = {userimage} alt = "userimage"/>
+        </div>
+
+        <ul>
+          <li> <Link to="/PastUpload"> PastUpload </Link> </li>
+          <li> <Link to="/UserProfile/SavedPost"> SavedPost </Link> </li>
+          <li> <Link to="/UserProfile/EditProfile"> Edit Profile </Link> </li>
+        </ul>
+      </div>
+      
+      </>
+
+
+      );
 }
 
 export default UserProfile;
