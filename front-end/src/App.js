24cadf0e47e47a03c786cb76bc198a06401e3595
tasnimahmed11 <ta1743@nyu.edu>
2022-10-18 15:29:40 -0400

    added prop values in the app.js for editprofile router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -56,7 +56,7 @@ function App() {
                                                       />
 
               {/* a route for the edit profile page */}
-              <Route path = "/UserProfile/EditProfile" element={<EditProfile/>} />
+              <Route path = "/UserProfile/EditProfile" element={<EditProfile fullname = "Foo Bar" email = "foobar@school.edu" tel = "+971234567"/>} />
 
               {/* a route for the saved post page */}
               <Route path = "/UserProfile/SavedPost" element={<SavedPost/>} />
--- front-end/src/pages/EditProfile.js
+++ front-end/src/pages/EditProfile.js
@@ -1,3 +1,4 @@
+import "./EditProfile.css"
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
@@ -17,9 +18,8 @@ function EditProfile(props) {
 
           <Box sx={{ width: '100%', height: "100%"}} className = "UserDetails">
             <Stack direction= "column" alignItems="center">
-            <TextField label="First Name" variant="filled" color="success" focused />
-            <TextField label="Last Name" variant="filled" color="success" focused />
-            <TextField label="Email ID" variant="filled" color="success" focused />
+            <TextField label="Full Name" defaultValue = {props.fullname} variant="filled" color="success" focused />
+            <TextField label="Email ID" defaultValue = {props.email} variant="filled" color="success" focused />
             <TextField label="Filled success" variant="filled" color="success" focused />
             </Stack>
           </Box>
