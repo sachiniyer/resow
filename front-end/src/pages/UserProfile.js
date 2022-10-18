68bd796c6f0443a57d6e1aaa100f67f0603aefa0
tasnimahmed11 <ta1743@nyu.edu>
2022-10-17 21:15:48 -0400

    Fixed the sizes of each div

--- front-end/src/pages/UserProfile.js
+++ front-end/src/pages/UserProfile.js
@@ -24,22 +24,24 @@ function UserProfile(props) {
     <>
       <div className="UserProfile">
 
-        <div className="NameImage">
+        <div className="TopPart">
           <img className="ProfilePicture" src = "https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg"
            alt = "userimage"/>
-           <h2> {props.fullname} </h2>
+           <h2 className="FullName"> {props.fullname} </h2>
+           <p className="Username"> @{props.username} </p>
         </div>
 
         <div className = "UserDetails">
-          <p> @{props.username} </p>
           <p> {props.email} </p>
-        
+          <p> Phone Number </p>
+          <p>  New York, USA </p>
         </div>
 
         <div className = "Buttons">
           <AllButtons link = "/UserProfile/EditProfile" text = "Edit Profile"/>
           <AllButtons link = "/PastUpload" text = "Past Uploads" />
           <AllButtons link = "/UserProfile/SavedPost" text = "Saved Posts" />
+          <AllButtons link = "/" text = "Sign Out" />
         </div>
 
       </div>
