ecdfdcc950881d28909ced88f48f48aa4d0db732
tasnimahmed11 <ta1743@nyu.edu>
2022-10-23 01:57:17 -0400

    removed props from UserProfile in app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -35,7 +35,7 @@ function App() {
               descriptions = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "/>} />
 
               {/* a route for the user profile page */}
-              <Route path = "/UserProfile" element={<UserProfile fullname = "Foo Bar" email = "foobar@school.edu" username = "foobar1" tel = "+9711234567" location = "New York, USA"/>} />
+              <Route path = "/UserProfile" element={<UserProfile />} />
 
               {/* a route for the messages page */}
               <Route path = "/Messages" element={<Messages/>} />
