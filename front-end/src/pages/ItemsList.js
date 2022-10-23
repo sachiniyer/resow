72b1c6065043640eb26acc59934eac3606f4fd1f
MinwuKim <minwu1221@gmail.com>
2022-10-23 10:44:47 -0400

    update item list page

--- front-end/src/pages/ItemsList.js
+++ front-end/src/pages/ItemsList.js
@@ -1,11 +1,29 @@
-import { Link } from "react-router-dom";
+import PostCard from '../components/Post';
+import { useEffect,useState } from 'react';
+import axios from "axios";
 
-function ItemList() {
+
+function ItemList(props) {
+
+  const [itemList,setItemList] = useState([]);
+
+  async function fetchData() {
+    const result = await axios(
+      "https://my.api.mockaroo.com/items/?key=59c3eda0"
+    );
+    setItemList(result.data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
     return (
-      <div className="ItemList">
-          <h1> Item List Page </h1>
-          <Link to="/ItemDetails"> Item Details </Link> { /*the link should be done with detail ID later on*/}
-      </div>
+      <>
+        {itemList && itemList.map((item) => (
+          <PostCard key={item.id} info = {item}/>
+        ))}
+      </>
     );
   }
   
