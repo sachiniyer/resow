861d29d2f25db062a0a3ebe00250c090e453ddc9
tasnimahmed11 <ta1743@nyu.edu>
2022-10-22 20:06:54 -0400

    removed css items

--- front-end/src/pages/EditProfile.js
+++ front-end/src/pages/EditProfile.js
@@ -2,7 +2,9 @@ import "./EditProfile.css"
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
 import TextField from '@mui/material/TextField';
+
 
 
 import InputAdornment from '@mui/material/InputAdornment';
@@ -12,10 +14,9 @@ import EmailIcon from '@mui/icons-material/Email';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 
-
 function TextFieldContainer(props){
   return(
-    <Box sx={{ border: 1, borderRadius: '15px', m: 1, p: 1, color:'grey.800', bgcolor:'#e5e4e2'}}> 
+    <Box sx={{ borderRadius: '7px', m: 1, p: 0.95, color:'grey.800', bgcolor:'#e5e4e2'}}> 
       <TextField label={props.label}
         InputProps={{startAdornment: ( <InputAdornment position="start"> {props.icon} </InputAdornment>),}} 
         variant="standard" 
@@ -29,19 +30,31 @@ function EditProfile(props) {
     return (
       <>
         <div className="EditProfile">
-          <div className="TopPart">
-            <img className="ProfilePicture" src = "https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg"
-            alt = "userimage"/>
-          </div>
 
-    
-            <Stack spacing={1} direction= "column" alignItems="center">
-              <TextFieldContainer label="Full Name" icon = <AccountCircle/> text = {props.fullname}/>
-              <TextFieldContainer label="Username" icon = <AlternateEmailIcon/> text = {props.username}/>
-              <TextFieldContainer label="Email ID" icon = <EmailIcon /> text = {props.email}/>
-              <TextFieldContainer label = "Phone Number" icon = <LocalPhoneIcon/> text = {props.tel} /> 
-              <TextFieldContainer label="Location" icon = <HomeRoundedIcon /> text = {props.location} />
+          <Box sx={{mt: 3, mb: 2}}>
+            <Stack direction="row" style={{ justifyContent: "center", display: "flex", p: 2}} spacing={1}>
+              <Avatar
+              alt="Profile Picture"
+              src="https://as2.ftcdn.net/v2/jpg/03/49/49/79/1000_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg"
+              sx={{ width: 120, height: 120 }}
+              />
             </Stack>
+          </Box>
+         
+            
+        
+
+        <div>
+          <Stack spacing={1} direction= "column" alignItems="center">
+            <TextFieldContainer label="Full Name" icon = <AccountCircle/> text = {props.fullname}/>
+            <TextFieldContainer label="Username" icon = <AlternateEmailIcon/> text = {props.username}/>
+            <TextFieldContainer label="Email ID" icon = <EmailIcon /> text = {props.email}/>
+            <TextFieldContainer label = "Phone Number" icon = <LocalPhoneIcon/> text = {props.tel} /> 
+            <TextFieldContainer label="Location" icon = <HomeRoundedIcon /> text = {props.location} />
+          </Stack>
+        </div>
+          
+          
 
           <Box sx={{m: 2}}>
             <Stack spacing={2} direction = "row" alignItems="center" justifyContent="center">
