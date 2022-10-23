284583ba79bee0220dbac787ec77a359dc5660a3
MinwuKim <minwu1221@gmail.com>
2022-10-23 17:19:04 -0400

    clean up homepage

--- front-end/src/pages/Homepage.js
+++ front-end/src/pages/Homepage.js
@@ -1,30 +1,16 @@
 import { Link } from "react-router-dom";
-import MessageCard from '../components/Message'
-import PostCard from '../components/Post'
-import PreviewWindow from '../components/PreviewWindow';
 import SearchBar from '../components/SearchBar';
 
 
 function Homepage() {
   return (
+    <>
     <div className="Homepage">
         <h1> Map Page </h1>
         <SearchBar></SearchBar>
         <Link to ="/Map/ItemsList"> List Pages </Link>
-        <MessageCard
-          img="/resowLogo.png"
-          name="user x"
-          date="mm.dd.yyyy"
-          message="when are you coming to pick the bunnie rabbits"
-        />
-        <PreviewWindow 
-          thumbnailURL = "/resowLogo.png"
-          profileURL = "/resowLogo.png"
-          sellerName = "Foo Barstein"
-          title = "Cute rabbit needs a new home"
-          location = "400 Broome St"
-        />
     </div>
+    </>
   );
 }
 
