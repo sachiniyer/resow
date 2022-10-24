623dda04d8a28fee941cb9e8cd05b038d9f51a7b
vicka1228 <bm3001@nyu.edu>
2022-10-24 02:58:32 -0400

    base code

--- front-end/src/pages/PastUpload.js
+++ front-end/src/pages/PastUpload.js
@@ -1,14 +1,49 @@
+import { Link } from "react-router-dom";
 
+import SearchBar from '../components/SearchBar';
+import PostCard from '../components/Post';
+import axios from "axios";
+import { useEffect,useState } from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
-function PastUploads() {
+function PastUpload(props) {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    async function fetchData() {
+
+      const result = await axios(
+        "https://my.api.mockaroo.com/items/?key=59c3eda0"
+      );
+
+      setData(result.data);
+    }
+
+    fetchData();
+  }, []);
+
     return (
-      <div className="PastUploads">
-          <h1> Past Upload Page </h1>
-      </div>
+      <>
+      <section className="PastUpload">
+        <br></br>
+        <SearchBar></SearchBar>
+        <br></br>
+        { data && data.map((item) => (
+          <PostCard 
+            key={item.id}  
+            info={item}
+          />
+        ))}
+      </section>
+
+      <Box sx={{position: "fixed", bottom: 20}}>
+            <Button component={Link} to="/UserProfile" variant="contained" color="success">Back to Profile</Button>
+      </Box>
+    </>
     );
   }
-  
-  export default PastUploads;
-  
-  
-  
+
+  export default PastUpload
