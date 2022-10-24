e3e8c37fed7c660bc0d25f2221b5d176c2632760
vicka1228 <bm3001@nyu.edu>
2022-10-24 02:32:37 -0400

    fixing a commit in master

--- front-end/src/pages/SavedPost.js
+++ front-end/src/pages/SavedPost.js
@@ -1,49 +1,14 @@
-import { Link } from "react-router-dom";
 
-import SearchBar from '../components/SearchBar';
-import PostCard from '../components/Post';
-import axios from "axios";
-import { useEffect,useState } from 'react';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
 
-function SavedPost(props) {
 
-  const [data, setData] = useState([]);
+function SavedPost() {
+  return (
+    <div className="SavedPost">
+        <h1> Saved Post Page </h1>
+    </div>
+  );
+}
 
-  useEffect(() => {
+export default SavedPost;
 
-    async function fetchData() {
 
-      const result = await axios(
-        "https://my.api.mockaroo.com/items/?key=59c3eda0"
-      );
-
-      setData(result.data);
-    }
-
-    fetchData();
-  }, []);
-
-    return (
-      <>
-      <section className="SavedPost">
-        <br></br>
-        <SearchBar></SearchBar>
-        <br></br>
-        { data && data.map((item) => (
-          <PostCard 
-            key={item.id}  
-            info={item}
-          />
-        ))}
-      </section>
-
-      <Box sx={{position: "fixed", bottom: 20}}>
-            <Button component={Link} to="/UserProfile.js" variant="contained" color="success">Back to Profile</Button>
-      </Box>
-    </>
-    );
-  }
-
-  export default SavedPost
