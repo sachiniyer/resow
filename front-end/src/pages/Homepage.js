20c7e2d54a1f791a62ffdc172d434bee3a134dfd
Sachin Iyer <sachinjiyer@gmail.com>
2022-10-23 20:33:55 -0400

    add all local commits before merge

--- front-end/src/pages/Homepage.js
+++ front-end/src/pages/Homepage.js
@@ -11,9 +11,14 @@ function Homepage() {
       new Feature({
         geometry: new Point([0, 0]),
         name: "some point"
+      }),
+      new Feature({
+        geometry: new Point([0, 1]),
+        name: "another point"
       })
     ]
     setFeatures(parsedFeatures)
+    console.log(parsedFeatures)
 
   }, [])
 
