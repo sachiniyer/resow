0b696e062fb656b5fd5ae5d2a240400f022c76ab
JacobLBaum <jacoblbaum@gmail.com>
2022-10-24 12:41:05 -0400

    create picture upload functionality in uploadItem page

--- front-end/src/pages/UploadItem.js
+++ front-end/src/pages/UploadItem.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import * as React from 'react';
+import { useEffect,useState } from 'react';
 import ImgCarousel from '../components/carousel/ImgCarousel';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -9,36 +10,79 @@ import TextField from '@mui/material/TextField';
 import PhotoCamera from '@mui/icons-material/PhotoCamera';
 
 
-function UploadItem() {
-  return (
-    <div className="UploadItem">
-        <h1> Upload Item Page</h1>
-        <Box sx={{marginTop:2}}></Box>
-        <ImgCarousel/>
-        <IconButton color="primary" aria-label="upload picture" component="label">
-          <input hidden accept="image/*" type="file" />
-          <PhotoCamera />
-        </IconButton>
-        <Box sx={{position: "fixed", bottom: 5, marginLeft:9}}>
-          <ButtonGroup variant="contained" aria-label="outlined success button group">
-            <Button color="success">Discard</Button>
-            <Button color="success">Post</Button>
-          </ButtonGroup>
-        </Box>
 
-        <Box>
-          <TextField fullWidth label="Post Title" id="fullWidth" />
-          <TextField
-            fullWidth
-            placeholder="Post Description"
-            multiline
-            rows={3}
-            maxRows={5}
-          />
-        </Box>
-        
-        <Link to ='/PastUpload'> PastUpload</Link>
-    </div>
+function UploadItem() {
+
+  const [itemPics,setItemPics] = useState([])
+
+  function handleUpload(event) {
+    console.log('Event:', event)
+    console.log('Event.target:', event.target)
+    console.log('Event.target.files:', event.target.files)
+
+    if (event.target.files[0]) {
+      let file = event.target.files[0];
+      let reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setItemPics([...itemPics, reader.result])
+      }
+    }
+  }
+
+  return (
+    <>
+      <Box sx={{width:{xs:0.9,sm:0.5,md: 0.3}, paddingTop:1}}>
+          <ImgCarousel imgList={itemPics}/>  
+      </Box>
+      <Box sx={{width:{xs:0.9,sm:0.5,md: 0.3}}}>
+          
+          <Box>
+            <h1> Upload Item Page</h1>
+          </Box>
+
+          <Box>
+            <IconButton color="primary" aria-label="upload picture" component="label">
+
+            <input
+              hidden
+              accept="image/*"
+              style={{ display: 'none' }}
+              id="raised-button-file"
+              multiple
+              type="file"
+              onChange={handleUpload}
+            />
+            <label htmlFor="raised-button-file">
+              <PhotoCamera />
+            </label>    
+            </IconButton>
+          </Box>
+    
+
+          
+
+          <Box>
+            <TextField fullWidth label="Post Title" id="fullWidth" />
+            <TextField
+              fullWidth
+              placeholder="Post Description"
+              multiline
+              rows={3}
+              maxRows={5}
+            />
+          </Box>
+
+          <Link to ='/PastUpload'> PastUpload</Link>
+      </Box>
+      <Box sx={{position: "fixed", bottom: 5}}>
+        <ButtonGroup variant="contained" aria-label="outlined success button group">
+          <Button href='/' color="success">Discard</Button>
+          <Button href='/PastUpload' color="success">Post</Button>
+        </ButtonGroup>
+      </Box>
+
+    </>
   );
 }
 
