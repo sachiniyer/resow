cc4b5f9413748d3bd304bd943b0a4d6c11a7b0e5
tasnimahmed11 <ta1743@nyu.edu>
2022-11-05 23:53:18 -0400

    user post request body added

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -8,9 +8,25 @@ router.get('/', (req,res) => {
 
 });
 
-router.post('/', (req,res)=> {
-    console.log(req.body) 
-});
+router.post('/', async (req,res)=> {
 
+    const user = new User({
+        fullname: req.body.fullname,
+        emailID: req.body.emailID,
+        password: req.body.password,
+        dob: req.body.dob,
+        phone: req.body.phone,
+        img: req.body.img
+    });
+
+    try {
+        const savedUser = await user.save();
+        res.json(savedUser);
+    }
+    catch (err) {
+        res.json({message: err})
+    }
+
+});
 
 module.exports = router;
