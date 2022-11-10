fced250e50b6b1249c4c65c41f7b484e782b0398
Sachin Iyer <sachinjiyer@gmail.com>
2022-11-09 19:20:56 -0500

    add some small error handling for map backend

--- back-end/routes/map.js
+++ back-end/routes/map.js
@@ -6,6 +6,10 @@ router.get('/', async (_, res) => {
     try {
         const posts = await Post.find()
         let retObj = {}
+        for (let i of posts) {
+            if (!(i.id && i.latitude && i.longitude))
+                throw "missing a field in a post"
+        }
         retObj.type = "FeatureCollection"
         retObj.features = []
         for (let i of posts) {
@@ -30,6 +34,8 @@ router.get('/feature', async (req, res) => {
     try {
         let post = await Post.findById(req.query.id)
         let retObj = {}
+        if (!(post.images && post.profileURL && post.title && post.latitude && post.longitude))
+            throw "missing a field"
         retObj.imgList = post.images
         retObj.profileURL = post.profileURL
         retObj.title = post.title
