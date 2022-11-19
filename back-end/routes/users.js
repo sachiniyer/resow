7c0c4faa3cb250eb47349e3df2ce365e265c1cf8
tasnimahmed11 <ta1743@nyu.edu>
2022-11-19 09:07:39 -0500

    added profile route and commented out userId route

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken") // used for authentication with JSON Web Tok
 const router = express.Router()
 const User = require("../models/userschema")
 
-const { createTokens } = require('./jwt-config')
+const { createTokens, validateToken } = require('./jwt-config')
 
 
 router.get('/', async (req, res) => {
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
     }
 })
 
-//have to remove this after fixing the tokenization
+/*//have to fis thix after the tokenization
 router.get('/:userId', async (req, res) => {
     try {
         const user = await User.findById(req.params.userId)
@@ -26,8 +26,7 @@ router.get('/:userId', async (req, res) => {
     catch (err) {
         res.json({message: err.message})
     }
-})
-
+})*/
 
  //route for adding a new user (user registration page)
 router.post('/register', async (req,res)=> {
@@ -54,7 +53,17 @@ router.post('/register', async (req,res)=> {
 
 })
 
-//login router to check if the entered details are correct or not (Log In Page)
+router.get('/profile', async (req, res) => {
+    try {
+        //const user = await User.findById(req.params.userId)
+        res.json("hello")
+    }
+    catch (err) {
+        res.json({message: err.message})
+    }
+})
+
+//login router to check if the entered details are correct or not (Log In Page) - AUTHORIZATION and AUTHENTICATION
 router.post('/logIn', async (req,res, )=> {
     const {emailID, password} = req.body;
 
@@ -79,6 +88,7 @@ router.post('/logIn', async (req,res, )=> {
     }
 
 })
+
 
 router.patch('/:userId', async (req, res) => {
     //route for updating a user profile (edit profile page)
@@ -111,6 +121,9 @@ router.delete('/:userId', async (req, res) => {
     }
 })
 
+
+
+//--------------------------- SAVE POST ROUTERS RELATED TO USER -------------------------------------------------
 router.get('/saved-posts/userId=:userId&postId=:postId', async (req, res) => {
 
     try {
