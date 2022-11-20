71e5c5597eab0f85d3b700ccfe8bef091b6707cb
tasnimahmed11 <ta1743@nyu.edu>
2022-11-19 22:11:50 -0500

    edited the jwtStrategy in jwt-config.js

--- back-end/routes/jwt-config.js
+++ back-end/routes/jwt-config.js
@@ -11,16 +11,12 @@ jwtOptions.secretOrKey = process.env.JWT_SECRET
 const jwtStrategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
     //console.log("JWT payload received", jwt_payload) // debugging
 
-    User.findOne({id: jwt_payload.id}, function(err, user) {
-        if (err) {
-            return next(err, false);
-        }
-        if (user) {
-            return next(null, user);
-        } else {
-            return next(null, false);
-        }
-    });
+    const user = User.findOne({id: jwt_payload.id})
+    if (user) {
+        return next(null, user);
+    } else {
+        return next(null, false);
+    }
 })
 
 module.exports = {
