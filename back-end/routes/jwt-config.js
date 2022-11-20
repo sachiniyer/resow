f49038b4dbeacc6b6920c0d4d6e68a2c6087d35f
tasnimahmed11 <ta1743@nyu.edu>
2022-11-19 22:02:59 -0500

    changed from done to next in jwt-config.js

--- back-end/routes/jwt-config.js
+++ back-end/routes/jwt-config.js
@@ -27,33 +27,3 @@ module.exports = {
     jwtOptions,
     jwtStrategy,
 }
-
-/*const {sign, verify} = require("jsonwebtoken")
-
-//this function creates a token and returns it to the place where it is being imported
-const createTokens = (user) => {
-    const accessToken = sign({emailID: user.emailID, id: user.id}, "jsonsecretToken")
-    return accessToken
-}
-
-const validateToken = (req, res, next) => {
-    const accessToken = req.cookies["access-token"]
-
-    if(!accessToken){ //don't have a access token then return this error
-        return res.status(400).json({error: "User not authenticated!"})
-    }
-
-    try{
-        const validateToken = verify(accessToken,"jsonsecretToken")
-        if(validateToken){
-            req.authenticated = true
-            return next()
-        }
-    }
-    catch(err){
-        res.json({message: err.message})
-    }
-}
-
-
-module.exports = {createTokens, validateToken}*/
