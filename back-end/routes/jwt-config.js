b43c3c0b02792c7355d065a03ee859858a64048c
tasnimahmed11 <ta1743@nyu.edu>
2022-11-19 22:02:43 -0500

    changed from done to next in jwt-config.js

--- back-end/routes/jwt-config.js
+++ back-end/routes/jwt-config.js
@@ -8,17 +8,17 @@ let jwtOptions = {}
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); //extracts the token string exclusing the bearer part from the header - also validates the token
 jwtOptions.secretOrKey = process.env.JWT_SECRET 
 
-const jwtStrategy = new JwtStrategy(jwtOptions, function (jwt_payload, done) {
+const jwtStrategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
     //console.log("JWT payload received", jwt_payload) // debugging
 
     User.findOne({id: jwt_payload.id}, function(err, user) {
         if (err) {
-            return done(err, false);
+            return next(err, false);
         }
         if (user) {
-            return done(null, user);
+            return next(null, user);
         } else {
-            return done(null, false);
+            return next(null, false);
         }
     });
 })
